5bfd5a5836c7a4f48ed37832b1b4d69a6b26b452
reginapf03 <125872270+reginapf03@users.noreply.github.com>
2023-05-20 20:49:14 +0700

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,7 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import '../styles/responsive.css';
-import App from './views/app';
+import App from './view/app';
 
 const app = new App({
   button: document.querySelector('#hamburgerButton'),
