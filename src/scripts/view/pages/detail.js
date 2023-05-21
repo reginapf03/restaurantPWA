7f9c58e2575fb02034e7f0157a2ef8aab8d87529
reginapf03 <125872270+reginapf03@users.noreply.github.com>
2023-05-21 19:56:57 +0700

    Update detail.js

--- src/scripts/view/pages/detail.js
+++ src/scripts/view/pages/detail.js
@@ -1,20 +1,20 @@
 import RestaurantDBSource from '../../data/restodb-source';
 import UrlParser from '../../routes/url-parser';
+import { createRestaurantDetailTemplate } from '../templates/template-creator';
 
 const Detail = {
-    async render() {
-      return `
+  async render() {
+    return `
       <div id="resto" class="resto"></div>
       `;
-    },
-   
-    async afterRender() {
-        const url = UrlParser.parseActiveUrlWithoutCombiner();
-        const detail = await RestaurantDBSource.detailRestaurant(url.id);
-        const restoContainer = document.querySelector('#resto');
-        restoContainer.innerHTML = createRestaurantDetailTemplate(detail);
-      
-    },
-  };
-   
-  export default Detail;
+  },
+
+  async afterRender() {
+    const url = UrlParser.parseActiveUrlWithoutCombiner();
+    const detail = await RestaurantDBSource.detailRestaurant(url.id);
+    const restoContainer = document.querySelector('#resto');
+    restoContainer.innerHTML = createRestaurantDetailTemplate(detail);
+  },
+};
+
+export default Detail;
