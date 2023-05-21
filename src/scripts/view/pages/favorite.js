fd93d484a18cabe10534b976d595ab3f51f6f115
reginapf03 <125872270+reginapf03@users.noreply.github.com>
2023-05-21 19:57:25 +0700

    Update favorite.js

--- src/scripts/view/pages/favorite.js
+++ src/scripts/view/pages/favorite.js
@@ -1,13 +1,1 @@
-const Favorite = {
-    async render() {
-      return `
-        <h2>Detail/h2>
-      `;
-    },
-   
-    async afterRender() {
-      
-    },
-  };
-   
-  export default Favorite;
+
