1f558999149bd71f858113ac03661eac816651b2
reginapf03 <125872270+reginapf03@users.noreply.github.com>
2023-05-21 19:56:12 +0700

    Update resto.js

--- src/scripts/view/pages/resto.js
+++ src/scripts/view/pages/resto.js
@@ -1,24 +1,23 @@
 import RestaurantDBSource from '../../data/restodb-source';
-import {createRestaurantItemTemplate} from '../templates/template-creator';
+import { createRestaurantItemTemplate } from '../templates/template-creator';
 
 const Resto = {
-    async render() {
-      return `
+  async render() {
+    return `
       <div class="headline-content">
       <h1 tabindex="0" class="title-content">Explore Restaurants</h1>
       <div id="resto" class="resto"></div>
     </div>
       `;
-    },
-   
-    async afterRender() {
-      const resto = await RestaurantDBSource.listRestaurants();
-      const restoContainer = document.querySelector('#resto');
-      resto.forEach((resto) => {
-      restoContainer.innerHTML += createRestaurantItemTemplate(resto);
+  },
+
+  async afterRender() {
+    const resto = await RestaurantDBSource.listRestaurants();
+    const restoContainer = document.querySelector('#resto');
+    resto.forEach((detail) => {
+      restoContainer.innerHTML += createRestaurantItemTemplate(detail);
     });
-      
-    },
-  };
-   
-  export default Resto;
+  },
+};
+
+export default Resto;
